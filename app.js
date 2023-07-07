ea68e0442069edeaae4aeb8f0d3887226eafe074
Rb2PtCl6 <ggmetalgg2@gmail.com>
2023-07-07 16:07:51 +0300

    upload-button-state

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(basicAuth({
 
 // Serve the admin HTML page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'admin-2.html'));
+  res.sendFile(path.join(__dirname, 'admin-3.html'));
 });
 
 // Endpoint to fetch user URLs
